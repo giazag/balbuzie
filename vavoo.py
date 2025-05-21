a2c1f8315d5b6a5e2a6857bf3cb5d825c9b14716
giazag <gian.zagaria99@gmail.com>
2025-05-21 20:28:09 +0200

    Update vavoo.py

--- vavoo.py
+++ vavoo.py
@@ -3,10 +3,10 @@ import json
 import re
 import os
 
-USREPG = ""
-BRANCHEPG = ""
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+USREPG = "giazag"
+BRANCHEPG = "balbuzie"
+MFPLINK = "https://giazag-idrogeno.hf.space"     # non mettere lo / finale al link
+MFPPSW = "olympus593"
 
 PROXY = f"{MFPLINK}/proxy/hls/manifest.m3u8?api_password={MFPPSW}&d="
 HEADER = "&h_user-agent=VAVOO/2.6&h_referer=https://vavoo.to/"
